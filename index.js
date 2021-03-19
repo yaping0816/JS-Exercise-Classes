74de80a1bb7844cf617b873c435eaf6135b16e4c
Yaping Liu <yaping.liu12@outlook.com>
2021-03-18 20:32:35 -0700

    finished task 5

--- index.js
+++ index.js
@@ -168,7 +168,22 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
+ class Student extends Lambdasian{
+   constructor(attrs){
+    super(attrs);
+    this.previousBackground = attrs.previousBackground;
+    this.className = attrs.className;
+    this.favSubjects = attrs.favSubjects;
+   }
+   listSubjects(){
+     return `Loving ${this.favSubjects.join()}`;
+   }
+   PRAssignment(subject){
+    return `${this.name} has submitted a PR for ${subject}`
+   }
+   sprintChallenge(subject){
+    return `${this.name} has begun sprint challenge on ${subject}`
+   }
      
  }
   
